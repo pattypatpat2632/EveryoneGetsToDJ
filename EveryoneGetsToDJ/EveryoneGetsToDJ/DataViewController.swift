6006ba58c42f01cc0b2d81005f84d115980de0fe
pattypatpat2632 <oleary.audio@gmail.com>
2017-06-07 17:47:17 -0400

    first commit

---
+++ EveryoneGetsToDJ/EveryoneGetsToDJ/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  EveryoneGetsToDJ
+//
+//  Created by Patrick O'Leary on 6/7/17.
+//  Copyright © 2017 Patrick O'Leary. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
